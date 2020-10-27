3764b071e82ad4c039dac472f5d4ffa6ee5e1297
Ketan Maheshwari <ketancmaheshwari@gmail.com>
2020-10-27 13:29:41 -0400

    fix a sorting issue in totsum

--- game.py
+++ game.py
@@ -10,15 +10,16 @@ def main():
 
 def init_deck():
 
-    d=[('S',i) for i in range(2,15)]\
+    d = [('S',i) for i in range(2,15)]\
     + [('D',i) for i in range(2,15)]\
     + [('H',i) for i in range(2,15)]\
-    +[('C',i) for i in range(2,15)]
+    + [('C',i) for i in range(2,15)]
 
     return d
 
 def deal(d,n):
-    p={}
+
+    p = {}
     for i in range(1, 4) :
         for j in range(1, n+1):
             # p is a dict{playernum:[card1, card2, card3]}
@@ -32,25 +33,25 @@ def deal(d,n):
     return p
 
 def select_winner(p):
-    totsum={}
-    color={}
-    sequence={}
-    triple={}
+    totsum = {}
+    color = {}
+    sequence = {}
+    triple = {}
 
     for k,v in p.items():
         v.sort(key = lambda x:x[1]) # sort cards so its easy to find sequence
         print (k, v)
 
-        totsum[k]=v[0][1]+v[1][1]+v[2][1] # sum total for each player if needed to decide winner
+        totsum[k] = v[0][1]+v[1][1]+v[2][1] # sum total for each player if needed to decide winner
         
         if v[0][0] == v[1][0] == v[2][0]:
-            color[k]=v
+            color[k] = v
 
         if v[0][1] == v[1][1] == v[2][1]:
-            triple[k]=v
+            triple[k] = v
 
         if v[0][1] == v[1][1]-1 and v[1][1] == v[2][1]-1:
-            sequence[k]=v
+            sequence[k] = v
 
 
     if triple:
@@ -60,8 +61,10 @@ def select_winner(p):
     if sequence:
         return next(iter(sequence))
 
-    {k: v for k, v in sorted(totsum.items(), key=lambda item: item[1])}
-    return next(iter(totsum))
+    # Sort the totsum and return the last value (which is highest)
+    totsum = {k: v for k, v in sorted(totsum.items(), key=lambda item: item[1])}
+    print (totsum)
+    return list(totsum)[-1]
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
