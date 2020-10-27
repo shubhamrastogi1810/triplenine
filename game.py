fd432908ac1c1f8f25b74ad4602c826c5718c4fc
Ketan Maheshwari <ketancmaheshwari@gmail.com>
2020-10-27 07:55:09 -0400

    deal the cards done

--- game.py
+++ game.py
@@ -6,12 +6,17 @@ def main():
 #    print(deck)
 
     play = deal(deck,10)
+    for i in play:
+        print(i,play[i])
 #    winner = select_winner(play)
 
 
 def init_deck():
 
-    d=[('S',i) for i in range(2,15)]+[('D',i) for i in range(2,15)]+[('H',i) for i in range(2,15)]+[('C',i) for i in range(2,15)]
+    d=[('S',i) for i in range(2,15)]\
+    + [('D',i) for i in range(2,15)]\
+    + [('H',i) for i in range(2,15)]\
+    +[('C',i) for i in range(2,15)]
 
     return d
 
@@ -19,8 +24,15 @@ def deal(d,n):
     p={}
     for i in range(1, 4) :
         for j in range(1, n+1):
-           # p is a dict{playernum:[card1, card2, card3]}
-           p[j]=d[randint(0,len(d))]
+            # p is a dict{playernum:[card1, card2, card3]}
+            nextnum = random.randint(0, len(d))
+            nextcard = d[nextnum]
+            del d[nextnum]
+            if j not in p:
+                p[j] = [nextcard]
+            else:
+                p[j].append(nextcard)
+    return p
 
 if __name__=='__main__':
     main()
