e3c045616b9b5aefc4aac73bc397418696b34050
Ketan Maheshwari <ketancmaheshwari@gmail.com>
2020-11-02 13:43:19 -0500

    compare max value for first and second cards

--- game.py
+++ game.py
@@ -57,7 +57,8 @@ def tiebreak(cards, reason):
     """
     Find maximum of the card set.
     """
-    doubledict={}
+    doubledict = {}
+
     if reason=='double' and len(cards)>1:
         for key, val in cards.items():
             # figure out the double value
@@ -65,6 +66,27 @@ def tiebreak(cards, reason):
             doubledict[dval]=key
         
         return sorted(doubledict.items())[-1][1], reason
+    
+#    if reason=='max':
+#        maxval = 0
+#        maxplayerdict1 = {}
+#        for key, val in cards.items():
+#            if val[2][0] >= maxval:
+#                maxval=val[2][0]
+#                maxplayerdict1[key]=val
+#
+#        if len(maxplayerdict1) > 1:
+#            maxval = 0
+#            maxplayerdict2 = {}
+#            for key, val in maxplayerdict1.items():
+#                if val[1][0] >= maxval:
+#                    maxval=val[1][0]
+#                    maxplayerdict2[key]=val
+#            
+#            return list(maxplayerdict2.keys())[0], reason
+#        else:
+#            return list(maxplayerdict1.keys())[0] , reason    
+#
 
     tot = {}
     for key, val in cards.items():
@@ -78,19 +100,23 @@ def select_winner(play):
     """
     Select the winner.
     """
-    totsum = {}
+    maxdict = {}
     color = {}
     sequence = {}
     coloredsequence = {}
     double = {}
     triple = {}
+    maxval = 0
+    maxkey = 0
 
     for k,val in play.items():
         val.sort(key = lambda x:x[0]) # sort cards so its easy to find sequence
         #print('{:2d} {}'.format(k, val))
 
-        # sum total for each player if needed to decide winner
-        totsum[k] = val[0][0] + val[1][0] + val[2][0]
+        # max card if needed to decide winner
+        if val[2][0] >= maxval:
+            maxval = val[2][0]
+            maxkey = k
 
         if val[0][1] == val[1][1] == val[2][1]:
             color[k] = val
@@ -126,10 +152,24 @@ def select_winner(play):
 
     if double:
         return tiebreak(double, "double")
+    
 
-    # Sort the totsum and return the last value (which is highest)
-    totsum = {k: v for k, v in sorted(totsum.items(), key=lambda item: item[1])}
-    return list(totsum)[-1], "Sum"
+    # check if there are more players holding the max card
+    for k,val in play.items():
+        if val[2][0] == maxval:
+            maxdict[k]=val
+    
+    # if there are more players holding the max card
+    if len(maxdict)>1:
+        maxval=0
+        maxkey=0
+        for k,val in maxdict.items():
+            if val[1][0] >= maxval:
+                maxval = val[1][0]
+                maxkey = k
+
+    return maxkey, "max"
+
 
 def print_play(play):
     display = {
